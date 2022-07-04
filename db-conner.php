4c587c6e8f126d81fd6328b4548f37b275a656a8
Joy <q39078273@gmail@gmail.com>
2022-07-04 23:36:49 +0800

    db-connect

---
+++ db-conner.php
@@ -0,0 +1,15 @@
+<?php  
+$servername = "localhost";
+$username = "admin";
+$password = "12345";
+$dbname = "music_tem4";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  	die("連線失敗: " . $conn->connect_error); 
+}else{
+    // echo"連線成功";
+}
+
+
+?>
