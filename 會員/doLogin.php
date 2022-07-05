6a4f13b5174ef1dd9167307e2c305ecbd850c194
Joy <q39078273@gmail.com>
2022-07-05 18:35:37 +0800

    user

--- 會員/doLogin.php
+++ 會員/doLogin.php
@@ -7,9 +7,9 @@ session_start();
 
 $account=$_POST["account"];
 $password=$_POST["password"];
-$password=md5("password");
+$password=md5($password);
 
-$sql="SELECT * FROM users WHERE  account='$account' AND password='$password'";
+$sql="SELECT * FROM backstage WHERE account='$account' AND password='$password'";
 
 $result=$conn->query($sql);
 $userExist=$result->num_rows;  //帳號存在
@@ -18,14 +18,17 @@ if($userExist>0){
     $row=$result->fetch_assoc();
     $user=[
         "id"=>$row["id"],
-        "account"=>$row["account"],
-        "password"=>$row["password"],
+        "name"=>$row["name"],
+        "account"=>$row["account"]
+        
     ];
-    
+    unset($_SESSION["error"]);
+    $_SESSION["user"]=$user;
     header("location: sign-up.php");
 
 }else{
     echo "帳號或密碼錯誤";
+    $_SESSION["error"]["message"]="帳號密碼錯誤";
     header("location: doLogin.php");
 }
 
