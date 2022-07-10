63a41132ee82638225bd5cb009e8f1c9dc15c0f6
Joy <q39078273@gmail@gmail.com>
2022-07-10 21:05:45 +0800

    users

--- nav.php
+++ nav.php
@@ -48,13 +48,13 @@
                 </div>
             </div>
             <li>
-                <a href="http://localhost/mfee27-team4-git/article/create-article.php" class="nav-link my-1">
+                <a href="http://localhost/mfee27-team4-git/article/articles.php" class="nav-link my-1">
                     <img class="mb-1 me-2" src="/mfee27-team4-git/icon/article-icon.svg" width="16" height="16"></img>
                     文章管理
                 </a>
             </li>
             <li>
-                <a href="#" class="nav-link my-1">
+                <a href="http://localhost/mfee27-team4-git/teacher/teachers.php" class="nav-link my-1">
                     <img class="mb-1 me-2" src="/mfee27-team4-git/icon/teacher-icon.svg" width="16" height="16"></img>
                     師資管理
                 </a>
