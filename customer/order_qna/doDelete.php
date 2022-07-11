1c63e45650099213f9304e0811c34ca904495386
HuangRueiYu <b98170022@gmail.com>
2022-07-12 03:44:52 +0800

    阿瑞更新

--- customer/order_qna/doDelete.php
+++ customer/order_qna/doDelete.php
@@ -1,7 +1,16 @@
 <?php
 require("../../db-connect.php");
 
-$sql="UPDATE users SET name='$name', phone='$phone', email='$email' WHERE id=$id"
-$conn->query($sql)
-$conn->close();
+$order_id=$_POST["order_id"];
+$arrayId=$_POST["arrayId"];
+print_r($arrayId);
+echo "<br>";
+
+foreach($arrayId as $id ){
+    $sql="UPDATE order_qna_detail SET valid=0 WHERE order_id=$order_id AND id=$id";
+    echo $sql;
+    $conn->query($sql);
+}
+$order_qna_id=$_POST["order_qna_id"];
+header("location: order_qna_detail.php?order_qna_id=".$order_qna_id);
 ?>
