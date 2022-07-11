952baaa5a61a36fdf2af07581875039b0cb2706d
Emmafan <emmafan1114@gmail.com>
2022-07-11 17:56:14 +0800

    文章管理

--- teacher/teacher-doUpdate.php
+++ teacher/teacher-doUpdate.php
@@ -3,56 +3,58 @@
 if (!isset($_POST["id"])) {
   echo "<script>alert('沒有師資資料'); location.href = 'teachers.php'; </script>";
   exit;
-}
-
-// 連結資料庫
-require("../db-connect.php");
-
-
-$id = $_POST["id"];
-$name = $_POST["name"];
-$field = $_POST["field"];
-$profile = $_POST["profile"];
-$video = $_POST["video"];
-$oldImage = $_POST["oldImage"];
-
-
-
-// 教授課程複選資料 組成陣列寫入 teacher資料表
-$courseId = array();
-// 取得 post courseId
-$courseId = $_POST["courseId"];
-// // 將陣列轉字串存入
-$courseIdArray = implode(',', $courseId);
-
-// 將資料寫入 teacher 資料表
-$sqlAll = "UPDATE teacher SET courses='$courseIdArray', field='$field', profile='$profile', video='$video' WHERE id=$id";
-$conn->query($sqlAll);
-
-
-
-if ($_FILES['newImage']['error'] == 0) {
-  if (move_uploaded_file($_FILES["newImage"]["tmp_name"], "../images/" . $_FILES["newImage"]["name"])) {
-    // 設定成系統時間 
-    $newImage = $_FILES["newImage"];
-    $newImageName = $_FILES["newImage"]["name"];
-    date_default_timezone_set('Asia/Taipei');
-    $now = date('Y-m-d H:i:s');
-    // 將關於圖片的文字資料傳入 images 資料表
-    $sqlImage = "UPDATE images SET image='$newImageName', upload_time='$now' WHERE name='$name'";
-    $conn->query($sqlImage);
-
-    $sqlTeacherImg = "UPDATE teacher SET image='$newImageName' WHERE id=$id";
-    $conn->query($sqlTeacherImg);
-  }
 } else {
-  $sqlOldImage = "UPDATE images SET image='$oldImage' WHERE name='$name'";
-  $conn->query($sqlOldImage);
 
-  $sqlOldImageTeacher = "UPDATE teacher SET image='$oldImage' WHERE id='$id'";
-  $conn->query($sqlOldImageTeacher);
+
+  // 連結資料庫
+  require("../db-connect.php");
+
+
+  $id = $_POST["id"];
+  $name = $_POST["name"];
+  $field = $_POST["field"];
+  $profile = $_POST["profile"];
+  $video = $_POST["video"];
+  $oldImage = $_POST["oldImage"];
+
+
+
+  // 教授課程複選資料 組成陣列寫入 teacher資料表
+  $courseName = array();
+  // 取得 post courseId
+  $courseName = $_POST["courseName"];
+  // // 將陣列轉字串存入
+  $courseNameArray = implode('、', $courseName);
+
+  // 將資料寫入 teacher 資料表
+  $sqlAll = "UPDATE teacher SET courses='$courseNameArray', field='$field', profile='$profile', video='$video' WHERE id=$id";
+  $conn->query($sqlAll);
+
+
+
+  if ($_FILES['newImage']['error'] == 0) {
+    if (move_uploaded_file($_FILES["newImage"]["tmp_name"], "../images/" . $_FILES["newImage"]["name"])) {
+      // 設定成系統時間 
+      $newImage = $_FILES["newImage"];
+      $newImageName = $_FILES["newImage"]["name"];
+      date_default_timezone_set('Asia/Taipei');
+      $now = date('Y-m-d H:i:s');
+      // 將關於圖片的文字資料傳入 images 資料表
+      $sqlImage = "UPDATE images SET image='$newImageName', upload_time='$now' WHERE name='$name'";
+      $conn->query($sqlImage);
+
+      $sqlTeacherImg = "UPDATE teacher SET image='$newImageName' WHERE id=$id";
+      $conn->query($sqlTeacherImg);
+    }
+  } else {
+    $sqlOldImage = "UPDATE images SET image='$oldImage' WHERE name='$name'";
+    $conn->query($sqlOldImage);
+
+    $sqlOldImageTeacher = "UPDATE teacher SET image='$oldImage' WHERE id='$id'";
+    $conn->query($sqlOldImageTeacher);
+  }
+
+
+  echo "<script>alert('師資修改成功'); location.href = 'teachers.php'; </script>";
+  $conn->close();
 }
-
-
-echo "<script>alert('師資修改成功'); location.href = 'teacher.php?id=$id'; </script>";
-$conn->close();
--- teacher/teachers.php
+++ teacher/teachers.php
@@ -2,6 +2,10 @@
 // 連結資料庫
 require("../db-connect.php");
 
+// 抓師資資料
+// $sql = "SELECT * FROM teacher  WHERE valid=1";
+// $result = $conn->query($sql);
+// $rows = $result->fetch_all(MYSQLI_ASSOC);
 
 // 抓課程商品資料
 $sqlCourseProduct = "SELECT * FROM course_product WHERE id AND valid=1";
@@ -15,24 +19,76 @@ if (isset($_GET["page"])) {
 }
 
 
+//search
+if (!isset($_GET["search"])) {
+    $search = "";
+    $sqlSearch = "";
+    $teacherCount = 0;
+} else {
+    $search = $_GET["search"];
+    $sqlSearch = "id LIKE '%$search%' || name LIKE '%$search%' || courses LIKE '%$search%' || field LIKE '%$search%' || profile LIKE '%$search%' AND";
+}
+
+// 排序
+$order = isset($_GET["order"]) ? $_GET["order"] : "";
+switch ($order) {
+    case 1:
+        $orderType = "id DESC";
+        break;
+
+    default:
+        $orderType = "id ASC";
+}
+
+//教學領域篩選
+$fieldOrderArray = ["琴鍵類音樂", "弦樂類音樂", "管樂類音樂", "熱音類音樂", "其他類音樂"];
+if (!isset($_GET["fieldOrder"])) {
+    $fieldOrder = "";
+    $fieldOrderType = "";
+    $teacherCount = 0;
+} else {
+    $fieldOrder = $_GET["fieldOrder"];
+    switch ($fieldOrder) {
+        case '琴鍵類音樂':
+            $fieldOrderType = "field='琴鍵類音樂' AND";
+            break;
+        case '弦樂類音樂':
+            $fieldOrderType = "field='弦樂類音樂' AND";
+            break;
+        case '管樂類音樂':
+            $fieldOrderType = "field='管樂類音樂' AND";
+            break;
+        case '熱音類音樂':
+            $fieldOrderType = "field='熱音類音樂' AND";
+            break;
+        case '其他類音樂':
+            $fieldOrderType = "field='其他類音樂' AND";
+            break;
+        default:
+            $fieldOrderType = "";
+    }
+}
+
+
 // 頁碼
-$sqlAll = "SELECT * FROM teacher  WHERE valid=1";
+$sqlAll = "SELECT * FROM teacher WHERE $sqlSearch valid=1";
 $resultAll = $conn->query($sqlAll);
 $teacherCount = $resultAll->num_rows;
 
 $perPage = 4;
 $startPage = ($page - 1) * $perPage;
-$sql = "SELECT * FROM teacher  WHERE valid=1 LIMIT $startPage, $perPage";
+// $sqlTeacher = "SELECT * FROM teacher WHERE $sqlSearch valid=1 ORDER BY $orderType LIMIT $startPage, $perPage";
+$sqlTeacher = "SELECT * FROM teacher WHERE $fieldOrderType $sqlSearch valid=1 ORDER BY $orderType LIMIT $startPage, $perPage";
 
-$result = $conn->query($sql);
-$pageTeacherCount = $result->num_rows;
-$teacherRows = $result->fetch_all(MYSQLI_ASSOC);
+$resultTeacher = $conn->query($sqlTeacher);
+$pageTeacherCount = $resultTeacher->num_rows;
+$teacherRows = $resultTeacher->fetch_all(MYSQLI_ASSOC);
 
-$startItem = ($page - 1) * $perPage;
+$startItem = ($page - 1) * $perPage + 1;
 $endItem = $page * $perPage;
 
-if ($endItem > $teacherCount) $endItem = $teacherCount;
-$totalPage = ceil($teacherCount / $perPage);
+if ($endItem > $pageTeacherCount) $endItem = $pageTeacherCount;
+$totalPage = ceil($pageTeacherCount / $perPage);
 
 ?>
 
@@ -86,7 +142,7 @@ $totalPage = ceil($teacherCount / $perPage);
                 <!-- 麵包屑 breadcrumb -->
                 <biv aria-label="breadcrumb">
                     <ol class="breadcrumb fw-bold">
-                        <li class="breadcrumb-item"><a href="#">首頁</a></li>
+                        <li class="breadcrumb-item"><a href="../home.php">首頁</a></li>
                         <li class="breadcrumb-item" aria-current="page">師資管理</li>
                     </ol>
                 </biv>
@@ -97,9 +153,11 @@ $totalPage = ceil($teacherCount / $perPage);
                 <!-- 內容 -->
                 <div class="container">
                     <div class="row">
-                        <form action="teacher-search.php" method="get">
+                        <form action="teachers.php" method="get">
+                            <input type="hidden" value="<?= $page ?>" name="page">
+                            <input type="hidden" value="<?= $order ?>" name="order">
                             <div class="row">
-                                <p class="col-8 m-auto">本頁 <?= $pageTeacherCount ?> 筆資料，總共 <?= $teacherCount ?> 筆資料</p>
+                                <p class="col-8 m-auto">目前 <?= $startItem ?> - <?= $endItem ?> 筆，總共 <?= $pageTeacherCount ?> 筆資料</p>
                                 <input class="col form-control me-3" type="text" name="search">
                                 <button class="col-1 btn btn-green" type="submit">
                                     <img class="bi pe-none mb-1" src="../icon/search-icon.svg" width="16" height="16"></img>
@@ -110,136 +168,127 @@ $totalPage = ceil($teacherCount / $perPage);
                     </div>
                 </div>
                 <hr>
-                <div class="container">
-                    <!-- 按鈕 -->
-                    <div class="d-flex justify-content-between">
-                        <div class="col">
-                            <a class="col btn btn-green me-2" href="teacher-create.php">
-                                <img class="mb-1" src="../icon/create-icon.svg" width="16" height="16"></img>
-                                新增師資
-                            </a>
-                        </div>
-                        <!-- 篩選還沒做 -->
-                        <form action="teacher-course-search.php" method="get">
-                            <div class="d-flex justify-content-between">
-                                <div class="me-3">
-                                    <select class="form-select" aria-label="Default select example" name="course">
-                                        <option selected value="">教授課程師資</option>
-                                        <?php foreach ($CourseProductRows as $row) : ?>
-                                            <option value="<?= $row["id"] ?>"><?= $row["course_name"] ?></option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                </div>
-                                <button class="btn btn-green" type="submit">
-                                    <!-- <img class="bi pe-none mb-1" src="../icon/search-icon.svg" width="16" height="16"></img> -->
-                                    篩選
+                <!-- 新增按鈕 -->
+                <div class="d-flex justify-content-between">
+                    <div class="col">
+                        <a class="col btn btn-green me-2" href="teacher-create.php">
+                            <img class="mb-1" src="../icon/create-icon.svg" width="16" height="16"></img>
+                            新增師資
+                        </a>
+                    </div>
+                    <!-- 排序、篩選按鈕 -->
+                    <div class="d-flex">
+                        <div class="me-2">
+                            <form action="teachers.php" method="get">
+                                <input type="hidden" value="<?= $page ?>" name="page">
+                                <input type="hidden" value="<?= $search ?>" name="search">
+                                <input type="hidden" value="<?= $order ?>" name="order">
+                                <input type="hidden" value="<?php if ($order == "") {
+                                                                echo "1";
+                                                            } ?>" name="order">
+                                <button class="btn  btn-khak me-3" type="submit">
+                                    師資編號排序
                                 </button>
-                            </div>
+                            </form>
+                        </div>
+                        <!-- 教學領域篩選 -->
+                        <form class="d-flex" action="teachers.php" method="get">
+                            <select class="col form-select me-2" aria-label="Default select example" name="fieldOrder">
+                                <option selected selected value="">教學領域</option>
+                                <?php foreach ($fieldOrderArray as $rowOrderArray) : ?>
+                                    <option value="<?= $rowOrderArray ?>"><?= $rowOrderArray ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <button class="col btn btn-grey" type="submit">
+                                篩選
+                            </button>
                         </form>
                     </div>
-                    <table class="table mt-2">
-                        <thead>
+                </div>
+                <table class="table mt-4">
+                    <thead>
+                        <tr>
+                            <th scope="col">編號</th>
+                            <th scope="col">師資照片</th>
+                            <th scope="col">師資姓名</th>
+                            <th scope="col">教學領域</th>
+                            <th scope="col" width="130">教授課程</th>
+                            <th scope="col" width="400">師資簡介</th>
+                            <th scope="col-2">管理操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- 撈每一筆資料 -->
+                        <?php foreach ($teacherRows as $row) : ?>
                             <tr>
-                                <th scope="col">編號</th>
-                                <th scope="col">師資照片</th>
-                                <th scope="col">師資姓名</th>
-                                <th scope="col">教學領域</th>
-                                <th scope="col">教授課程</th>
-                                <th scope="col" width="350">師資簡介</th>
-                                <th scope="col">管理操作</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- 撈每一筆資料 -->
-                            <?php foreach ($teacherRows as $row) : ?>
-                                <tr>
-                                    <td><?= $row["id"] ?></td>
-                                    <td><img class="object-cover  rounded" src="
+                                <td><?= $row["id"] ?></td>
+                                <td><img class="object-cover  rounded" src="
                         <?php if (empty($row["image"])) {
-                                    // 如果沒有照片就顯示頭像icon
-                                    echo "../images/img-icon.svg";
-                                } else {
-                                    // 如果有照片就顯示上傳的照片
-                                    $teacherImage = $row["image"];
-                                    echo "../images/$teacherImage";
-                                }
+                                // 如果沒有照片就顯示頭像icon
+                                echo "../images/img-icon.svg";
+                            } else {
+                                // 如果有照片就顯示上傳的照片
+                                $teacherImage = $row["image"];
+                                echo "../images/$teacherImage";
+                            }
                         ?>
                         "></td>
-                                    <td><?= $row["name"] ?></td>
-                                    <td>
-                                        <?php
-                                        switch ($row["field"]) {
-                                            case '1':
-                                                echo "琴鍵類音樂";
-                                                break;
-                                            case '2':
-                                                echo "弦樂類音樂";
-                                                break;
-                                            case '3':
-                                                echo "管樂類音樂";
-                                                break;
-                                            case '4':
-                                                echo "熱音類音樂";
-                                                break;
-                                            case '5':
-                                                echo "其他類音樂";
-                                                break;
-                                        }
-                                        ?>
-                                    </td>
-                                    <td>
-                                        <?php
-                                        $coursesArray = explode(",", $row["courses"]);
-                                        foreach ($coursesArray as $key => $value) :
-                                            $sqlCourseName = "SELECT course_name FROM course_product WHERE id=$value";
-                                            $resultCourseName = $conn->query($sqlCourseName);
-                                            $rowsCourseName = $resultCourseName->fetch_array(MYSQLI_ASSOC);
-                                        ?>
-                                            <div>
-                                                <?= $rowsCourseName["course_name"]; ?>
-                                            </div>
-                                        <?php endforeach; ?>
-                                    </td>
-                                    <td align="left">
-                                        <p class="ellipsis">
-                                            <?= $row["profile"] ?>
-                                        </p>
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-grey me-3" type="button" href="teacher.php?id=<?= $row["id"] ?>">
-                                            <img class="bi pe-none mb-1" src="../icon/read-icon.svg" width="16" height="16"></img>
-                                            詳細
-                                        </a>
-                                        <a class="btn btn-khak" type="button" href="teacher-update.php?id=<?= $row["id"] ?>">
-                                            <img class="bi pe-none mb-1" src="../icon/update-icon.svg" width="16" height="16"></img>
-                                            修改
-                                        </a>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                                <td><?= $row["name"] ?></td>
+                                <td><?= $row["field"] ?></td>
+                                <td align="left"><?= $row["courses"]; ?></td>
+                                <td align="left">
+                                    <p class="ellipsis">
+                                        <?= $row["profile"] ?>
+                                    </p>
+                                </td>
+                                <td>
+                                    <div class="d-flex">
+                                        <form class="col" action="teacher.php" method="post">
+                                            <input type="hidden" value="<?= $page ?>" name="page">
+                                            <input type="hidden" value="<?= $search ?>" name="search">
+                                            <input type="hidden" value="<?= $order ?>" name="order">
+                                            <input type="hidden" value="<?= $row["id"] ?>" name="id">
+                                            <button class="btn btn-green" type="submit">
+                                                <img class="bi pe-none mb-1" src="../icon/read-icon.svg" width="16" height="16"></img>
+                                                詳細
+                                            </button>
+                                        </form>
+                                        <form class="col" action="teacher-edit.php" method="post">
+                                            <input type="hidden" value="<?= $page ?>" name="page">
+                                            <input type="hidden" value="<?= $search ?>" name="search">
+                                            <input type="hidden" value="<?= $order ?>" name="order">
+                                            <input type="hidden" value="<?= $row["id"] ?>" name="id">
+                                            <button class="btn btn-khak" type="submit">
+                                                <img class="bi pe-none mb-1" src="../icon/update-icon.svg" width="16" height="16"></img>
+                                                修改
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
 
-                    <!-- 頁碼 -->
-                    <div aria-label="Page navigation example text-end" class="d-flex mt-5  justify-content-center">
-                        <ul class="pagination">
-                            <li class="page-item">
-                                <!-- <a class="page-link" href="#" aria-label="Previous">
+                <!-- 頁碼 -->
+                <div aria-label="Page navigation example text-end" class="d-flex mt-4  justify-content-center">
+                    <ul class="pagination">
+                        <!-- <li class="page-item"> -->
+                        <!-- <a class="page-link" href="#" aria-label="Previous">
                                     <span aria-hidden="true">&laquo;</span>
                                 </a> -->
-                            </li>
-                            <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
-                                <li class="page-item"><a class="page-link <?php if ($page == $i) echo "active"; ?>" href="teachers.php?page=<?= $i ?>"><?= $i ?></a></li>
-                            <?php endfor; ?>
-                            <li class="page-item">
-                                <!-- <a class="page-link" href="#" aria-label="Next">
+                        <!-- </li> -->
+                        <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
+                            <li class="page-item"><a class="page-link <?php if ($page == $i) echo "active"; ?>" href="teachers.php?page=<?= $i ?>&search=<?= $search ?>&order=<?= $order ?>&fieldOrderType=<?= $fieldOrderType ?>"><?= $i ?></a></li>
+                        <?php endfor; ?>
+                        <!-- <li class="page-item"> -->
+                        <!-- <a class="page-link" href="#" aria-label="Next">
                                     <span aria-hidden="true">&raquo;</span>
                                 </a> -->
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- 頁碼 end -->
+                        <!-- </li> -->
+                    </ul>
                 </div>
+                <!-- 頁碼 end -->
 
 
         </div>
