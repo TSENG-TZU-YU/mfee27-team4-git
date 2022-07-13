aba399b8a0d6bdf756efd4b25200c86f7a0bc21b
Joy <q39078273@gmail.com>
2022-07-13 14:56:01 +0800

    auto member

--- coupon/coupons.php
+++ coupon/coupons.php
@@ -185,8 +185,7 @@ $totalPage = ceil($couponCountAll / $perPage);
                                         <td><?= $row["several_times"] ?></td>
                                         <td><?= $row["min_price"] ?></td>
                                         <td>
-                                            <a class="btn btn-grey me-3" type="" href="coupon.php?
-                        id=<?= $row["id"] ?>">
+                                            <a class="btn btn-grey me-3" type="" href="coupon.php?id=<?= $row["id"] ?>">
                                                 <img class="bi pe-none mb-1" src="../icon/read-icon.svg" width="16" height="16"></img>
                                                 詳細
                                             </a>
